7bb72974e212fde90d459727b6530b8c6239a190
Banerjee, Subhodip <Subhodip.Banerjee@Honeywell.com>
2018-07-12 14:21:57 +0530

    Project Created.

---
+++ CoredataExperiment/CoredataExperiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoredataExperiment
+//
+//  Created by Banerjee, Subhodip on 12/07/18.
+//  Copyright © 2018 Subhodip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
